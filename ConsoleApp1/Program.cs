3886705c690b7cd6fe9a0aeafd2b68e16cb779b8
Mubarak Aloraifan <aloraifan505@hotmail.com>
2024-04-14 13:41:42 +0300

    Add project files.

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+using static System.Console;
+//Task 1.1
+WriteLine("---------<Task 1>---------");
+
+int age = 22;
+double balance = 23332.23;
+    string name = "Mubarak";
+    bool married = false;
+    
+    Console.WriteLine(age);
+    Console.WriteLine(balance);
+    Console.WriteLine(name);
+    Console.WriteLine(married);
+
+//Task 1.2
+
+    int number = 5;
+    double result = Convert.ToDouble(number);
+    Console.WriteLine($"{result:.00}");
+    
+    
+    char grade = 'A';
+    Console.WriteLine($"{name},your garde is {grade}!!!");
+
+
+WriteLine("---------<Task 2>---------");
+
+
+//Task 2.1
+
+var studentName = "Mubarak";
+var GPA = 3.82;
+var favSubject = "Math";
+
+//Task 2.2
+
+double height = 6.2;
+double width = 3.5;
+
+Console.WriteLine($"The area of the rectangle is {height * width}");
+
+
+var fawazname = "Fawaz";
+fawazname = "Youesf";
+
+Console.WriteLine(fawazname);
+
